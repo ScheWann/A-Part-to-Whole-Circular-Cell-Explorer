80ad52036322fbcaa093dd15c15c5a07d82b0551
ScheWann <92758590+ScheWann@users.noreply.github.com>
2024-06-04 14:21:33 -0400

    add tissue picture as the svg background

--- Frontend/src/components/pieChart.js
+++ Frontend/src/components/pieChart.js
@@ -2,6 +2,8 @@ import React, { useEffect, useRef } from "react";
 import * as d3 from "d3";
 import data from "../data/piechart.csv";
 import scaleJson from "../data/scalefactors_json.json";
+import hiresTissuePic from '../data/tissue_hires_image.png'
+import lowresTissuePic from '../data/tissue_lowres_image.png'
 
 const officialColors = ["#EF3819", "#F39A2E", "#A4F93F", "#41F63D", "#4BF7A7", "#459CF9", "#3821F6", "#A031F7", "#F23C9D"]
 
@@ -9,22 +11,25 @@ export const PieChart = () => {
     const svgRef = useRef(null);
 
     // lowres scale factor
-    // const scalef = scaleJson["tissue_lowres_scalef"];
-    // const spotDiameter = scaleJson["spot_diameter_fullres"];
-    // const radius = (spotDiameter * scalef / 2);
-
-    // hires scale factor
-    const scalef = scaleJson["tissue_hires_scalef"] - 0.07;
+    const scalef = scaleJson["tissue_lowres_scalef"];
     const spotDiameter = scaleJson["spot_diameter_fullres"];
     const radius = (spotDiameter * scalef / 2);
+
+    // hires scale factor
+    // const scalef = scaleJson["tissue_hires_scalef"] - 0.07;
+    // const spotDiameter = scaleJson["spot_diameter_fullres"];
+    // const radius = (spotDiameter * scalef / 2);
     
     useEffect(() => {
         const svgElement = d3.select(svgRef.current);
         svgElement.selectAll("*").remove();
 
         const svg = svgElement
-            .attr("width", 800)
-            .attr("height", 800);
+            .attr("width", 600)
+            .attr("height", 600);
+        
+        svg.append("image")
+            .attr("href", lowresTissuePic)
 
         const arc = d3.arc().innerRadius(0).outerRadius(radius);
         const pie = d3.pie().value(d => parseFloat(d[1]));
