73fd0f7b61eec7b39896e699bfcf28310b59068e
ScheWann <92758590+ScheWann@users.noreply.github.com>
2024-06-20 14:29:12 -0500

    finish kosara encoding

--- RedesignChallenge/Frontend/src/components/kosaraChart.js
+++ RedesignChallenge/Frontend/src/components/kosaraChart.js
@@ -1,11 +1,20 @@
 import React, { useEffect, useRef, useState } from "react";
 import * as d3 from "d3";
-import data from "../data/piechart.csv";
+import data from "../data/kosaraChart.csv";
 import scaleJson from "../data/scalefactors_json.json";
-import hiresTissuePic from '../data/tissue_hires_image.png';
 import lowresTissuePic from '../data/tissue_lowres_image.png';
 
-const officialColors = ["#EF3819", "#F39A2E", "#A4F93F", "#41F63D", "#4BF7A7", "#459CF9", "#3821F6", "#A031F7", "#F23C9D"];
+const officialColors = {
+    X1: "#EF3819",
+    X2: "#F39A2E",
+    X3: "#A4F93F",
+    X4: "#41F63D",
+    X5: "#4BF7A7",
+    X6: "#459CF9",
+    X7: "#3821F6",
+    X8: "#A031F7",
+    X9: "#F23C9D"
+}
 
 export const KosaraChart = () => {
     const svgRef = useRef(null);
@@ -15,6 +24,39 @@ export const KosaraChart = () => {
     const scalef = scaleJson["tissue_lowres_scalef"];
     const spotDiameter = scaleJson["spot_diameter_fullres"];
     const radius = (spotDiameter * scalef / 2);
+
+    function generateKosaraPath(pointX, pointY, angles) {
+        let paths = [];
+        let lastStartPointX, lastStartPointY, lastEndPointX, lastEndPointY = 0;
+        let filteredAngles = angles.filter(item => item[1] !== 0);
+
+        filteredAngles.forEach((angle, index) => {
+            let startpointX = pointX - radius * Math.sin((45 + angle[1]) * Math.PI / 180);
+            let startpointY = pointY - radius * Math.cos((45 + angle[1]) * Math.PI / 180);
+            let endpointX = pointX - radius * Math.sin((45 - angle[1]) * Math.PI / 180);
+            let endpointY = pointY - radius * Math.cos((45 - angle[1]) * Math.PI / 180);
+    
+            let path = '';
+
+            if (index === 0) {
+                path = `M ${startpointX} ${startpointY} A ${radius} ${radius} 0 0 0 ${endpointX} ${endpointY} A ${radius} ${radius} 0 0 0 ${startpointX} ${startpointY} Z`;
+            } 
+            else if (index === filteredAngles.length - 1) {
+                path = `M ${lastStartPointX} ${lastStartPointY} A ${radius} ${radius} 0 0 0 ${lastEndPointX} ${lastEndPointY} A ${radius} ${radius} 0 1 1 ${lastStartPointX} ${lastStartPointY} Z`;
+            } 
+            else {
+                path = `M ${lastStartPointX} ${lastStartPointY} A ${radius} ${radius} 0 0 0 ${lastEndPointX} ${lastEndPointY} A ${radius} ${radius} 0 0 1 ${endpointX} ${endpointY} A ${radius} ${radius} 0 0 1 ${startpointX} ${startpointY} A ${radius} ${radius} 0 0 1 ${lastStartPointX} ${lastStartPointY} Z`;
+            }
+    
+            paths.push({path, color: officialColors[angle[0]]});
+
+            lastStartPointX = startpointX;
+            lastStartPointY = startpointY;
+            lastEndPointX = endpointX;
+            lastEndPointY = endpointY;
+        })
+        return paths;
+    }
 
     useEffect(() => {
         const svgElement = d3.select(svgRef.current);
@@ -57,25 +99,31 @@ export const KosaraChart = () => {
                 X7: +d.X7,
                 X8: +d.X8,
                 X9: +d.X9
+            },
+            angles: {
+                X1: +d.X1_angle,
+                X2: +d.X2_angle,
+                X3: +d.X3_angle,
+                X4: +d.X4_angle,
+                X5: +d.X5_angle,
+                X6: +d.X6_angle,
+                X7: +d.X7_angle,
+                X8: +d.X8_angle,
+                X9: +d.X9_angle
             }
         })).then(data => {
             contentGroup.selectAll("g").remove();
             if (showPieCharts) {
                 data.forEach((d) => {
-                    const ratios = Object.entries(d.ratios);
-                    console.log(d.ratios);
-                    const arcs = d3.pie().value(d => parseFloat(d[1]))(ratios);
-                    const color = d3.scaleOrdinal(officialColors);
-
+                    const angles = Object.entries(d.angles);
                     const group = contentGroup.append("g")
-                        .attr("transform", `translate(${d.x}, ${d.y})`);
+                    const paths = generateKosaraPath(d.x, d.y, angles);
 
-                    group.selectAll('path')
-                        .data(arcs)
-                        .enter()
-                        .append('path')
-                        .attr('d', d3.arc().innerRadius(0).outerRadius(radius))
-                        .attr('fill', (d, index) => color(index));
+                    paths.forEach(({ path, color }) => {
+                        group.append('path')
+                            .attr('d', path)
+                            .attr('fill', color);
+                    });
                 });
             } else {
                 data.forEach((d) => {
