e539b670b7a2ca734c407347e5c7ea624e11553e
ScheWann <92758590+ScheWann@users.noreply.github.com>
2024-07-03 11:46:30 -0500

    Update process.py

--- RedesignChallenge/Backend/process.py
+++ RedesignChallenge/Backend/process.py
@@ -1,38 +1,45 @@
 import pandas as pd
 from scipy.io import mmread
-from scipy import sparse 
+from scipy import sparse
 import numpy as np
+import scanpy as sc
 from scipy.optimize import fsolve
 import h5py
 
 
 feature_matrix = mmread("../Data/FeatureMatrix.mtx")
 geneList = pd.read_csv("../Data/Genes.csv")
-positions = pd.read_csv('../Data/SpotPositions.csv')
-kosaraData = pd.read_csv('../Data/kosaraChart.csv')
+positions = pd.read_csv("../Data/SpotPositions.csv")
+kosaraData = pd.read_csv("../Data/kosaraChart.csv")
 
-with h5py.File('../Data/Filtered_feature_bc_matrix.h5', 'r') as file:
-    genes = [name.decode('utf-8') for name in file['matrix/features/name'][:]]
-    barcodes = [barcode.decode('utf-8') for barcode in file['matrix/barcodes'][:]]
-    
-    data = file['matrix/data'][:]
-    indices = file['matrix/indices'][:]
-    indptr = file['matrix/indptr'][:]
-    shape = file['matrix/shape'][:]
+# with h5py.File('../Data/Filtered_feature_bc_matrix.h5', 'r') as file:
+#     genes = [name.decode('utf-8') for name in file['matrix/features/name'][:]]
+#     barcodes = [barcode.decode('utf-8') for barcode in file['matrix/barcodes'][:]]
 
-    matrix = sparse.csc_matrix((data, indices, indptr), shape=shape)
+#     data = file['matrix/data'][:]
+#     indices = file['matrix/indices'][:]
+#     indptr = file['matrix/indptr'][:]
+#     shape = file['matrix/shape'][:]
 
-    expression_data = pd.DataFrame(matrix.toarray(), index=genes, columns=barcodes).transpose()
-    expression_data.fillna(0, inplace=True)
+#     matrix = sparse.csc_matrix((data, indices, indptr), shape=shape)
 
-# expression_data = pd.DataFrame(feature_matrix, index=positions['barcode'], columns=genes['gene'])
-# expression_data.columns.name = None
+#     expression_data = pd.DataFrame(matrix.toarray(), index=genes, columns=barcodes).transpose()
+#     expression_data.fillna(0, inplace=True)
+
+adata = sc.read_10x_h5("../Data/Filtered_feature_bc_matrix.h5")
+
+adata.var_names_make_unique()
+
+expression_data = adata.to_df()
+
 
 def get_gene_list():
-    return list(geneList['gene'])
+    return list(geneList["gene"])
+
 
 def get_gene_expression(gene):
     return expression_data[gene]
+
 
 def get_kosara_data():
     return kosaraData
