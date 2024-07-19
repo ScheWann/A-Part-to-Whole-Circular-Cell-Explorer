cc0b9413467cbc8586b92ee390fd6db7b9233442
ScheWann <92758590+ScheWann@users.noreply.github.com>
2024-07-19 16:01:46 -0500

    give a tooltip to violinchart(not working now)

--- RedesignChallenge/Frontend/src/components/gradientLegend.js
+++ RedesignChallenge/Frontend/src/components/gradientLegend.js
@@ -80,12 +80,13 @@ export const GradientLegend = ({ min, max, selectedGene, colorScaleType, showKos
             .attr("x", 0)
             .attr("y", height + 15)
             .text(!showKosaraCharts && min ? (Number.isInteger(min) ? min : min.toFixed(3)) : "")
+            // .text(min)
             .attr("font-size", "12px");
 
         svg.append("text")
             .attr("x", width)
             .attr("y", height + 15)
-            .text(!showKosaraCharts && max ? (Number.isInteger(max) ? max : max.toFixed(2)) : "")
+            .text(!showKosaraCharts && max ? (Number.isInteger(max) ? max : max.toFixed(3)) : "")
             .attr("font-size", "12px")
             .style("text-anchor", "end");
 
--- RedesignChallenge/Frontend/src/components/violinPlot/VerticalViolinShape.tsx
+++ RedesignChallenge/Frontend/src/components/violinPlot/VerticalViolinShape.tsx
@@ -1,5 +1,5 @@
 import * as d3 from "d3";
-import React from "react";
+import React, { useState, useRef } from "react";
 
 type VerticalViolinShapeProps = {
   data: number[];
@@ -18,15 +18,22 @@ export const VerticalViolinShape = ({
   fill,
   smoothing,
 }: VerticalViolinShapeProps) => {
+  const [tooltipContent, setTooltipContent] = useState<string | null>(null);
+  const tooltip = useRef<null | d3.Selection<HTMLDivElement, unknown, HTMLElement, any>>(null);
   const min = Math.min(...data);
   const max = Math.max(...data);
-
   const binBuilder = d3
     .bin()
     .domain([min, max])
     .value((d) => d);
   const bins = binBuilder(data);
-
+  if (!tooltip.current) {
+    tooltip.current = d3
+      .select("body")
+      .append("div")
+      .attr("class", "violinPlotTooltip")
+      .style("opacity", 0);
+  }
   const biggestBin = Math.max(...bins.map((b) => b.length));
 
   const wScale = d3
@@ -45,13 +52,26 @@ export const VerticalViolinShape = ({
 
   const areaPath = areaBuilder(bins);
   return (
-    <path
-      d={areaPath ? areaPath : ""}
-      opacity={1}
-      stroke="black"
-      fill={fill}
-      fillOpacity={1}
-      strokeWidth={1}
-    />
+    <>
+      <path
+        d={areaPath ? areaPath : ""}
+        opacity={1}
+        stroke="black"
+        fill={fill}
+        fillOpacity={1}
+        strokeWidth={1}
+        onMouseMove={(event) => {
+          const info = `Min: ${min}, Max: ${max}`;
+          setTooltipContent(info);
+          tooltip.current?.html(info)
+            .style("left", `${event.pageX + 10}px`)
+            .style("top", `${event.pageY - 28}px`)
+            .style("opacity", 0.9);
+        }}
+        onMouseOut={() => {
+          tooltip.current?.style("opacity", 0);
+        }}
+      />
+    </>
   );
 };
